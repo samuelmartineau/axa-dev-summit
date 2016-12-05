3f87b751377df1edeefa79b04bcd7500bdd58c9f
Samuel Martineau <samuelmartineau@IWN.local>
2016-12-05 16:03:24 +0100

    remove unused file

--- quote/feedbackCalculator.spec.js
+++
@@ -1,7 +0,0 @@
-const expect = require('chai').expect;
-const feedbackCalculator = require('../feedback/feedbackCalculator');
-
-describe("First Round", () => {
-  it('first test', () => {
-  });
-});
