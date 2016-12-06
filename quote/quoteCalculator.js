da190a88219ab8ee1fa820aa8556c96aa6070dac
CRUCHON Gilles <gilles.cruchon@axa.fr>
2016-12-06 12:37:53 +0100

    New rules

--- quote/quoteCalculator.js
+++ quote/quoteCalculator.js
@@ -3,6 +3,7 @@ const countries = require('../country');
 
 const minimalWeek = 7;
 const payDayOfTheWeek = 4;
+const majorityAge = 18;
 const quotes = {
     BASIC: 1.8,
     EXTRA: 2.4,
@@ -14,8 +15,62 @@ const optionsRef = {
     MEDICAL: 72,
     SCUBA: 36,
     SPORTS: 25,
-    YOGA: -3,
-    SPORTS: 5,
+    YOGA: -3
+}
+
+function isYoungAdult( age ){
+  return ( age >= 18 && age <= 24 );
+}
+
+function familyComposition( travellerAges ){
+  return travellerAges.reduce((acc, age) => {
+        if (isNaN(age)) {
+          throw 'issue';
+        }
+        if (age < 18) {
+            acc.kids ++;
+        } else {
+            acc.kids ++;
+        }
+        if( isYoungAdult(age) ){
+          acc.youngAdult ++;
+        }
+        return acc
+    }, {"kids": 0, "adults": 0, "youngAdult": 0}); 
+}
+
+function isTravellingAlone( family ){
+  return ( family.kids + family.adult == 1);
+}
+
+function tooManyKids( family ){
+  return ( family.adults < family.kids );
+}
+
+function hasFiveYoungAdults( family ){
+  return ( family.adults >= 5 );
+}
+
+function discountFamilyComposition( family ){
+  var discount = 1;
+  
+  if( isTravellingAlone( family ) ) {
+    discount *= 1.05;
+  }
+  
+  if( hasManyYoungAdults( family ) ) {
+    discount *= 0.9;
+  }
+
+  if( tooManyKids( family ) ){
+    discount *= 1.15;
+  } else {
+    //well balanced family
+    if( family.kids > 1 ){
+      discount *= 0.80;
+    }    
+  }
+  return discount;
 }
 
 function getNumberOfDays( daysNumber ){
@@ -74,6 +129,9 @@ module.exports = (form) => {
         }
         return acc
     }, 0);
+    const family = familyComposition( travellerAges );
 
-    return Math.round((quotes[cover.toUpperCase()] * countryFee  * ageFees * daysNumber + optionsAmount) * 100) / 100;
+    quoteWithoutDiscount = Math.round((quotes[cover.toUpperCase()] * countryFee  * ageFees * daysNumber + optionsAmount) * 100) / 100;
+    quote = quoteWithoutDiscount * discountFamilyComposition( family );
+    return quoteWithoutDiscount;
 }
